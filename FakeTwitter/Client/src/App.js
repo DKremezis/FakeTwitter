bea915f397019947ae77a9abb286634a72684bf8
Syed Javaid <s.java458@gmail.com>
2018-09-18 15:45:24 -0400

    Adding updated files including App, Homepage

--- FakeTwitter/Client/src/App.js
+++ FakeTwitter/Client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Login from './containers/Login/Login';
+import Homepage from './containers/Homepage/Homepage';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login/>
+        {/* <Login/> */}
+        <Homepage/>
       </div>
     );
   }
