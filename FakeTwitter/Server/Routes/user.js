40fa5b4b5148b7cbc715e73b492d4b1a7313f57f
Shao Eric Hu <erichu11364@gmail.com>
2018-09-18 14:52:20 -0400

    connected react native to backend

--- FakeTwitter/Server/Routes/user.js
+++ FakeTwitter/Server/Routes/user.js
@@ -3,25 +3,30 @@ const router = express.Router();
 const dbm = require('./../DatabaseManager');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const myPlaintextPassword = 's0//P4$$w0rD';
 
-router.post('/', (req, res) =>  {
+router.post('/', (req, res) => {
+  console.log(req.body);
 
-    bcrypt.hash(req.body.password, saltRounds).then( function(hash) {
-        req.body.password = hash;
-        console.log(hash);
-        console.log(req.body);
+  bcrypt
+    .hash(req.body.password, saltRounds)
+    .then(function(hash) {
+      req.body.password = hash;
+      console.log(hash);
+      console.log(req.body);
 
-        dbm.insertStatement(req.body, 'usersTable').then((result) => {
-            console.log('Data was inserted');
-        }).catch((error) => {
-            console.log("something went wrong in the router file")
+      dbm
+        .insertStatement(req.body, 'usersTable')
+        .then(result => {
+          res.status(200).send({ exists: true });
+        })
+        .catch(error => {
+          console.log('something went wrong in the router file');
         });
-
+    })
+    .catch(error => {
+      console.log('here: ' + error);
     });
+});
 
-        
-
-})
-
-module.exports = router
+module.exports = router;
